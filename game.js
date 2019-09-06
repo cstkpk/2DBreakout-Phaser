cc706d81d933f2ab937cc5b551054ed12c3b5e38
Cara Sunberg <cnsunberg@gmail.com>
2019-09-06 14:21:09 -0400

    Added paddle:
    *Paddle image renders
    *Paddle control works with mouse (or touch)
    *Paddle default position is the middle of the game world

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 
 // Game variables
 let ball;
+let paddle;
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -11,6 +12,7 @@ function preload() {
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = "rgb(50, 50, 50)";
     game.load.image("ball", "img/ball.png");
+    game.load.image("paddle", "img/paddle.png");
 };
 
 function create() {
@@ -20,8 +22,14 @@ function create() {
     ball.body.collideWorldBounds = true;
     ball.body.bounce.set(1);
     ball.body.velocity.set(150, 150);
+    paddle = game.add.sprite(game.world.width * 0.5, game.world.height-5, "paddle");
+    paddle.anchor.set(0.5, 1);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    paddle.body.immovable = true;
 };
 
 function update() {
-
+    game.physics.arcade.collide(ball, paddle);
+    // Sets paddle position to input position (mouse or touch) or defaults to the center
+    paddle.x = game.input.x || game.world.width * 0.5;
 };

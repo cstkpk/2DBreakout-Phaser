ba01f2a60398e271ca6db69574ab80c82451207f
Cara Sunberg <cnsunberg@gmail.com>
2019-09-06 13:14:01 -0400

    Added scaling to game.js

--- game.js
+++ game.js
@@ -3,7 +3,10 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 });
 
 function preload() {
-
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.stage.backgroundColor = "rgb(50, 50, 50)";
 };
 function create() {
 

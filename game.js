9cb77bc994d330765a5938b0da71209f53ab67c8
Cara Sunberg <cnsunberg@gmail.com>
2019-09-06 12:59:46 -0400

    Added the basic code for initializing Phaser
    *Tried using the phaser-ce npm package, but Phaser was not defined in game.js. What was going on there?
    *Got it to work by downloading the min.js. Would just be good to understand how to use the npm version.

---
+++ game.js
@@ -0,0 +1,13 @@
+const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+    preload: preload, create: create, update: update
+});
+
+function preload() {
+
+};
+function create() {
+
+};
+function update() {
+
+};
